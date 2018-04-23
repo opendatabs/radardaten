ddd39e7d7a1efc02f203a093102248512d31348c
Endtner <endtner@campus.unibe.ch>
2018-04-23 11:30:40 +0200

    set up bootrap and some components

--- front_end/src/app/app-routing.module.ts
+++ front_end/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainComponent} from "./main/main.component";
+import {MapViewComponent} from "./main/map-view/map-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'mapView', pathMatch: 'full' },
+  { path: 'home', component: MainComponent },
+  { path: 'mapView', component: MapViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front_end/src/app/app.module.ts
+++ front_end/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MapComponent } from './main/map/map.component';
+import { MapViewComponent } from './main/map-view/map-view.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavbarComponent,
+    MapComponent,
+    MapViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
