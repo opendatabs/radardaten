82f3886d6d9c3821ef76211761857fb606e13f1e
alkraeuchi <alkraeuchi@users.noreply.github.com>
2018-11-16 19:44:33 +0100

    add async catch to cron

--- config/cron.js
+++ config/cron.js
@@ -1,5 +1,6 @@
 const mysqldump = require('mysqldump');
 const fs = require('fs');
+const fsp = require('fs/promises');
 
 
 let env = {};
@@ -14,16 +15,25 @@ module.exports.cron = {
     cronJob: {
         // ['seconds', 'minutes', 'hours', 'dayOfMonth', 'month', 'dayOfWeek']
         schedule: '0 */2 * * * *',
-        // schedule: '0 0 */2 * * *', // Every night at 2AM
+        // schedule: '0 0 0 * * *', // Every night at 2AM
         onTick: function () {
-            let file = require('path').resolve(sails.config.appPath, './download/radarDump.sql')
+            let file = require('path').resolve(sails.config.appPath, './download/radarDump.sql');
 
-            fs.writeFile(file, '', (err) => {
-                if (err) throw err;
-                console.log('Created new empty file');
-                console.log('Writing new MYSQL Dump...');
-                mysqldump({ connection, dumpToFile: file });
-            });
+            try {
+                await fsp.writeFile(file, '', () => {
+                    console.log('Writing new MYSQL Dump...');
+                    mysqldump({ connection, dumpToFile: file });
+                });
+                console.info('Successfully created MYSQL Dump');
+            } catch (error) {
+                console.error(error);
+            }
+            // fs.writeFile(file, '', (err) => {
+                // if (err) throw err;
+                // console.log('Writing new MYSQL Dump...');
+                // mysqldump({ connection, dumpToFile: file });
+            // });
+
             // let promise = new Promise((resolve, reject) => {
             // const result = mysqldump({ connection });
             // resolve(result);
