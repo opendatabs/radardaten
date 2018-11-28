d03548a673d4af2a9e59e11e2c77c3121b2a99d8
Jonas Bieri <jonas.bieri@gmail.com>
2018-11-28 15:34:55 +0000

    Introduce bash variables

--- scipts/dbupdate.sh
+++ scipts/dbupdate.sh
@@ -1,6 +1,10 @@
 # (1) loads mysql dump from web app, (2) imports sql dump into opendata database, (3) exports table radar to tsv, (4) exports table record to tsv. 
 # replace username and Password in the following command to be run as a cron job
-wget -O public_html/verkehrspolizei/mysqldump.sql https://username:password@radardaten.fdn.iwi.unibe.ch/data/getmysqldump \
-&& mysql -u opendata_verkehr -pPassword opendata_verkehrspolizeiradar < public_html/verkehrspolizei/mysqldump.sql \
-&& mysql -u opendata_verkehr -pPassword --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from radar;" > public_html/verkehrspolizei/radar.tsv \
-&& mysql -u opendata_verkehr -pPassword --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from record;" > public_html/verkehrspolizei/record.tsv 
+webappuser=username \
+&& webappw=password \
+&& sqluser=opendata_verkehr \
+&& sqlpw=password \
+&& wget -O public_html/verkehrspolizei/mysqldump.sql https://$webappuser:$webappw@radardaten.fdn.iwi.unibe.ch/data/getmysqldump \
+&& mysql -u $sqluser -p$password opendata_verkehrspolizeiradar < public_html/verkehrspolizei/mysqldump.sql \
+&& mysql -u $sqluser -p$password --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from radar;" > public_html/verkehrspolizei/radar.tsv \
+&& mysql -u $sqluser -p$password --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from record;" > public_html/verkehrspolizei/record.tsv 
