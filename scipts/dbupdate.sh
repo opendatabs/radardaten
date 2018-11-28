50ccb4aab0aec1a450c7d1d87e53dace408a4bc5
Jonas Bieri <jonas.bieri@gmail.com>
2018-11-28 15:41:44 +0000

    Fix variable name

--- scipts/dbupdate.sh
+++ scipts/dbupdate.sh
@@ -5,9 +5,9 @@ webappuser=username \
 && sqluser=opendata_verkehr \
 && sqlpw=password \
 && wget -O public_html/verkehrspolizei/mysqldump.sql https://$webappuser:$webappw@radardaten.fdn.iwi.unibe.ch/data/getmysqldump \
-&& mysql -u $sqluser -p$password opendata_verkehrspolizeiradar < public_html/verkehrspolizei/mysqldump.sql \
-&& mysql -u $sqluser -p$password --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from radar;" > public_html/verkehrspolizei/radar.tsv \
-&& mysql -u $sqluser -p$password --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from record;" > public_html/verkehrspolizei/record.tsv 
+&& mysql -u $sqluser -p$sqlpw opendata_verkehrspolizeiradar < public_html/verkehrspolizei/mysqldump.sql \
+&& mysql -u $sqluser -p$sqlpw --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from radar;" > public_html/verkehrspolizei/radar.tsv \
+&& mysql -u $sqluser -p$sqlpw --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from record;" > public_html/verkehrspolizei/record.tsv 
 
 # or as a oneliner: 
-# webappuser=username && webappw=password && sqluser=opendata_verkehr && sqlpw=password && wget -O public_html/verkehrspolizei/mysqldump.sql https://$webappuser:$webappw@radardaten.fdn.iwi.unibe.ch/data/getmysqldump && mysql -u $sqluser -p$password opendata_verkehrspolizeiradar < public_html/verkehrspolizei/mysqldump.sql && mysql -u $sqluser -p$password --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from radar;" > public_html/verkehrspolizei/radar.tsv && mysql -u $sqluser -p$password --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from record;" > public_html/verkehrspolizei/record.tsv 
+# webappuser=username && webappw=password && sqluser=opendata_verkehr && sqlpw=password && wget -O public_html/verkehrspolizei/mysqldump.sql https://$webappuser:$webappw@radardaten.fdn.iwi.unibe.ch/data/getmysqldump && mysql -u $sqluser -p$sqlpw opendata_verkehrspolizeiradar < public_html/verkehrspolizei/mysqldump.sql && mysql -u $sqluser -p$sqlpw --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from radar;" > public_html/verkehrspolizei/radar.tsv && mysql -u $sqluser -p$sqlpw --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from record;" > public_html/verkehrspolizei/record.tsv 
