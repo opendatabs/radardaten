c307a3587252149282eb4e49250f1b1274212b1f
Jonas Bieri <jonas.bieri@gmail.com>
2018-11-29 07:58:07 +0000

    Truncate tables before importing, do not stop on errors

--- scipts/dbupdate.sh
+++ scipts/dbupdate.sh
@@ -5,9 +5,11 @@ webappuser=username \
 && sqluser=opendata_verkehr \
 && sqlpw=password \
 && wget -O public_html/verkehrspolizei/mysqldump.sql https://$webappuser:$webappw@radardaten.fdn.iwi.unibe.ch/data/getmysqldump \
-&& mysql -u $sqluser -p$sqlpw opendata_verkehrspolizeiradar < public_html/verkehrspolizei/mysqldump.sql \
+&& mysql -u $sqluser -p$sqlpw opendata_verkehrspolizeiradar -e "TRUNCATE TABLE radar; TRUNCATE TABLE record;" \
+&& mysql -u $sqluser -p$sqlpw --force opendata_verkehrspolizeiradar < public_html/verkehrspolizei/mysqldump.sql > public_html/verkehrspolizei/dbrestore.log \
 && mysql -u $sqluser -p$sqlpw --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from radar;" > public_html/verkehrspolizei/radar.tsv \
 && mysql -u $sqluser -p$sqlpw --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from record;" > public_html/verkehrspolizei/record.tsv 
 
 # or as a oneliner: 
-# webappuser=username && webappw=password && sqluser=opendata_verkehr && sqlpw=password && wget -O public_html/verkehrspolizei/mysqldump.sql https://$webappuser:$webappw@radardaten.fdn.iwi.unibe.ch/data/getmysqldump && mysql -u $sqluser -p$sqlpw opendata_verkehrspolizeiradar < public_html/verkehrspolizei/mysqldump.sql && mysql -u $sqluser -p$sqlpw --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from radar;" > public_html/verkehrspolizei/radar.tsv && mysql -u $sqluser -p$sqlpw --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from record;" > public_html/verkehrspolizei/record.tsv 
+#webappuser=username && webappw=password && sqluser=opendata_verkehr && sqlpw=password && wget -O public_html/verkehrspolizei/mysqldump.sql https://$webappuser:$webappw@radardaten.fdn.iwi.unibe.ch/data/getmysqldump && mysql -u $sqluser -p$sqlpw opendata_verkehrspolizeiradar -e "TRUNCATE TABLE radar; TRUNCATE TABLE record;" && mysql -u $sqluser -p$sqlpw --force opendata_verkehrspolizeiradar < public_html/verkehrspolizei/mysqldump.sql > public_html/verkehrspolizei/dbrestore.log && mysql -u $sqluser -p$sqlpw --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from radar;" > public_html/verkehrspolizei/radar.tsv && mysql -u $sqluser -p$sqlpw --column-names=TRUE opendata_verkehrspolizeiradar -e "SELECT * from record;" > public_html/verkehrspolizei/record.tsv 
+
