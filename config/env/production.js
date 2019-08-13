37ceb5af2278d82a03d95ea41659e65c49d1dc43
Janik <janik.endtner@iwi.unibe.ch>
2019-08-13 08:39:41 +0200

    configuration for prod changed

--- config/env/production.js
+++ config/env/production.js
@@ -21,6 +21,14 @@
 
 module.exports = {
 
+  dumpConnection: {
+    url: local.localDB
+  },
+  dumpUser: {
+    username: local.localDUMPUSERNAME,
+    password: local.localDUMPUSERPASSWORD,
+  },
+
 
   /**************************************************************************
   *                                                                         *
@@ -363,23 +371,11 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
-    internalEmailAddress: 'support@example.com',
-
-    // mailgunDomain: 'mg.example.com',
-    // mailgunSecret: 'key-prod_fake_bd32301385130a0bafe030c',
-    // stripeSecret: 'sk_prod__fake_Nfgh82401348jaDa3lkZ0d9Hm',
-    //--------------------------------------------------------------------------
-    // /\   OR, to avoid checking them in to version control, you might opt to
-    // ||   set sensitive credentials like these using environment variables.
-    //
-    // For example:
-    // ```
-    // sails_custom__mailgunDomain=mg.example.com
-    // sails_custom__mailgunSecret=key-prod_fake_bd32301385130a0bafe030c
-    // sails_custom__stripeSecret=sk_prod__fake_Nfgh82401348jaDa3lkZ0d9Hm
-    // ```
-    //--------------------------------------------------------------------------
+    baseUrl: 'https://radardaten.fdn.iwi.unibe.ch',
+    authCredentials: {
+      username: local.authUSER,
+      password: local.authPASSWORD
+    },
 
   },
 
